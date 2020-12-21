70e36216f5a151e49677625ae3d61c5013eea4a0
Leo Kukharau <kuhorev.leonid@gmail.com>
2020-12-20 17:27:51 -0800

    minor changes

--- random_team.py
+++ random_team.py
@@ -4,34 +4,33 @@ import random
 def stringify(L: list[list[str]]) -> str:
     output = ''
     for pair in L:
-        output += ' - '.join(pair)
-        output += '\n'
+        output += ' - '.join(pair) + '\n'
 
     return output
 
 
-def get_random_pairs(students: list, students_in_group: int = 2) -> str:
-    """Divide the given list of students into the given number of groups. 
+def get_random_pairs(students: list, students_per_group: int = 2) -> str:
+    """Divide the given list of students into groups where each group contains the given number of students. All extra students will be added to the last group. 
 
     Args:
         students (list): List of students
-        students_in_group (int, optional): Number of students per group. Defaults to 2.
+        students_per_group (int, optional): Number of students per group. Defaults to 2.
 
     Returns:
         str: Students divided into groups
     """
 
     random.shuffle(students)
-    number_of_groups = len(students) // students_in_group - 1
+    number_of_groups = len(students) // students_per_group - 1
     output = []
     group = 0
 
     while group <= number_of_groups:
         if group != number_of_groups:
             output.append(
-                students[group * students_in_group:(group * students_in_group) + students_in_group])
+                students[group * students_per_group:(group * students_per_group) + students_per_group])
         else:
-            output.append(students[group * students_in_group:])
+            output.append(students[group * students_per_group:])
         group += 1
 
     return stringify(output)
